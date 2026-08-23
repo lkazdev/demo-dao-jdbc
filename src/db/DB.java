42c888f7757d83a55c5bc9164141b9060450c393
lkazdev <lkaz.dev@gmail.com>
2026-08-23 15:32:45 -0300

    FindById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,9 +3,7 @@ package db;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -42,6 +40,26 @@ public class DB {
             return props;
         } catch (IOException e) {
             throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
         }
     }
 
